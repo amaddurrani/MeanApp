299d467d7d7ff21abd24d38d3e98f119b1dc96a1
amad durrani <amadatiq786@gmail.com>
2019-08-19 19:02:54 +0500

    first commit

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.0.6.
 
+
+## How To Run
+0. Add Mongo db credentials in '\backend\app.js'
+1. Run Nodejs Server 'nodemon server.js'
+2. Run Angular App 'ng serve --open'
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
---
+++ backend/app.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const postsRoutes = require("./routes/posts");
+const userRoutes = require("./routes/user");
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/posts',{useNewUrlParser: true }, (err) => {
+    if (!err)
+        console.log('MongoDB connection succeeded.');
+    else
+        console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2));
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join("backend/images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/posts", postsRoutes);
+app.use("/api/user", userRoutes);
+
+module.exports = app;
